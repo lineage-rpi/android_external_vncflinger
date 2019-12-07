d5ac0fc6e207e656d9ea2a1308001253facdb261
maxwen <max.weninger@gmail.com>
2019-12-07 01:23:56 +0100

    fix build errors

--- Android.mk
+++ Android.mk
@@ -10,9 +10,6 @@ LOCAL_SRC_FILES := \
     src/VirtualDisplay.cpp \
     src/main.cpp
 
-#LOCAL_SRC_FILES += \
-#    aidl/org/chemlab/IVNCService.aidl
-
 LOCAL_C_INCLUDES += \
     $(LOCAL_PATH)/src \
     external/tigervnc/common \
@@ -23,6 +20,7 @@ LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libgui \
     libjpeg \
+    liblog \
     libssl \
     libui \
     libutils \
@@ -32,10 +30,9 @@ LOCAL_STATIC_LIBRARIES += \
     libtigervnc
 
 LOCAL_CFLAGS := -DVNCFLINGER_VERSION="1.0"
-LOCAL_CFLAGS += -Ofast -Werror -std=c++11 -fexceptions
+LOCAL_CFLAGS += -Ofast -Werror -fexceptions -Wno-implicit-fallthrough
 
 #LOCAL_CFLAGS += -DLOG_NDEBUG=0
-#LOCAL_CXX := /usr/bin/include-what-you-use
 
 LOCAL_INIT_RC := etc/vncflinger.rc
 
